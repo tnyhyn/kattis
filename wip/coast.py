30b8159706e6c6216ee58243c550352c93c2f722
TH <tnyhyn@gmail.com>
2019-11-13 18:50:07 -0500

    finished coast

--- wip/coast.py
+++ wip/coast.py
@@ -1,6 +1,11 @@
 from sys import stdin, setrecursionlimit
-setrecursionlimit(2000)
+setrecursionlimit(10000000)
+sea = 0
 
+
+def printer(grid):
+    for i in grid:
+        print(i)
 
 def coast(grid):
     p = 0
@@ -8,23 +13,32 @@ def coast(grid):
         for j in range(len(grid[0])):
             # if grid[i][j] == 0:
             s = set()
-            if is_sea(grid,i,j,s) and grid[i][j] == 0:
-                p += dfs(grid, i, j)
-    #             print(grid)
-    #             print("i: {} | j: {} | p: {}".format(i,j,p))
+            if  grid[i][j] == 0:
+                if is_sea(grid,i,j,s, False):
+                    p += dfs(grid, i, j)
+                    # printer(grid)
+                    # print("i: {} | j: {} | p: {}".format(i,j,p))
+                else:
+                    s = set() 
+                    is_sea(grid,i,j,s, True)
+    # printer(grid)
     # print("p: {} | edges: {}".format(p, edges(grid)))
+    # print("sea: {} ".format(sea))
     print(p + edges(grid))
 
-def is_sea(grid, i, j, v):
+def is_sea(grid, i, j, v, place):
+    global sea
+    sea += 1
     if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == -1:
         return False
     if (i,j) in v: return False
     v.add((i,j))
     if grid[i][j] == 1:
         return False
+    if place: grid[i][j] = 1
     if i == 0 or i == len(grid)-1 or j == 0 or j == len(grid[0])-1:
         return True
-    if is_sea(grid,i+1,j,v) or is_sea(grid,i-1,j,v) or is_sea(grid,i,j+1,v) or is_sea(grid,i,j-1,v):
+    if is_sea(grid,i+1,j,v,place) or is_sea(grid,i-1,j,v,place) or is_sea(grid,i,j+1,v,place) or is_sea(grid,i,j-1,v,place):
         return True
 
 def edges(grid):
@@ -50,4 +64,5 @@ grid = []
 for i in range(int(l)):
     r = [int(i) for i in list(stdin.readline().rstrip())]
     grid.append(r)
-coast(grid)
+if int(w) == 0 or int(l) == 0: print(0)
+else: coast(grid)
